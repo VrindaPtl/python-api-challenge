0d3a145670cf3769d6091bf85092ebce6c05712c
VrindaPtl <155428439+VrindaPtl@users.noreply.github.com>
2024-04-22 19:25:47 +1000

    updated

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,9 +1,9 @@
 # OpenWeatherMap API Key
-weather_api_key = "34019506a14f335ceb1a6eb9d2198750"
+weather_api_key = "My_API_Key"
 
 # Geoapify API Key
-geoapify_api_key = "166bc8b0db244610a9262921fd316232"
+geoapify_api_key = "My_API_Key"
 
 # GMap API Key
-gmap_api_key = "AIzaSyBSDSvvrNi-y2KAMxJM4JQquGUc4S60bw8"
+gmap_api_key = "My_API_Key"
 
